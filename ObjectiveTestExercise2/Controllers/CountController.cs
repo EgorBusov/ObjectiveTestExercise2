4427ebe60da4bdbeacb7b1fe62a6c196389b5da4
EgorBusov <sgyxa.ru@mail.ru>
2023-09-07 18:17:16 +0500

    first commit

---
+++ ObjectiveTestExercise2/Controllers/CountController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ObjectiveTestExercise2.Services;
+
+namespace ObjectiveTestExercise2.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class CountController : ControllerBase
+    {
+
+        [HttpGet("GetCount")]
+        public int GetCount()
+        {
+            return CounterService.GetCount();
+        }
+
+        [Route("AddCount/{number}")]
+        [HttpGet("{number}")]
+        public IActionResult AddCount(int number)
+        {
+            try
+            {
+                CounterService.AddCount(number);
+                return Ok();
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+    }
+}
---
+++ ObjectiveTestExercise2/Services/CounterService.cs
@@ -0,0 +1,35 @@
+﻿namespace ObjectiveTestExercise2.Services
+{
+    public static class CounterService
+    {
+        private static readonly ReaderWriterLockSlim _rwLock = new ReaderWriterLockSlim();
+
+        private static int _count = 0;
+
+        public static int GetCount()
+        {
+            try
+            {
+                _rwLock.EnterReadLock();
+                return _count;
+            }
+            finally
+            {
+                _rwLock.ExitReadLock();
+            }
+        }
+
+        public static void AddCount(int count)
+        {
+            try
+            {
+                _rwLock.EnterWriteLock();
+                _count += count;
+            }
+            finally
+            {
+                _rwLock.ExitWriteLock();
+            }
+        }
+    }
+}
